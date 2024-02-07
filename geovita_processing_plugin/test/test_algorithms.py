1537750d9716f9ca67917a7f3a95572247fe13a2
Daniel <naturogfrihet@gmail.com>
2024-02-07 14:47:39 +0100

    update test and add test badge to readme

--- geovita_processing_plugin/test/test_algorithms.py
+++ geovita_processing_plugin/test/test_algorithms.py
@@ -58,7 +58,7 @@ class TestBegrensSkadeExcavation(unittest.TestCase):
             'SHORT_TERM_SETTLEMENT': True,
             'EXCAVATION_DEPTH': 10.0,
             'SETTLEMENT_ENUM': 1, #index
-            'LONG_TERM_SETTLEMENT': False,
+            'LONG_TERM_SETTLEMENT': True,
             'RASTER_ROCK_SURFACE': self.raster_rock_surface_layer,
             'POREPRESSURE_ENUM': 1, #index
             'POREPRESSURE_REDUCTION': 50,
