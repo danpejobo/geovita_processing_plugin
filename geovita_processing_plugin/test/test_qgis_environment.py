0f126fe1dac57db9ea57e294e5781c31d13b5c47
Daniel <naturogfrihet@gmail.com>
2024-02-09 19:15:10 +0100

    update testcase

--- geovita_processing_plugin/test/test_qgis_environment.py
+++ geovita_processing_plugin/test/test_qgis_environment.py
@@ -8,16 +8,15 @@
      (at your option) any later version.
 
 """
-__author__ = 'tim@linfiniti.com'
-__date__ = '20/01/2011'
-__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
-                 'Disaster Reduction')
+__author__ = "tim@linfiniti.com"
+__date__ = "20/01/2011"
+__copyright__ = "Copyright 2012, Australia Indonesia Facility for " "Disaster Reduction"
 
 import unittest
-from qgis.core import (
-    QgsProviderRegistry)
+from qgis.core import QgsProviderRegistry, QgsCoordinateReferenceSystem
 
 from .utilities import get_qgis_app
+
 QGIS_APP = get_qgis_app()
 
 
@@ -28,8 +27,24 @@ class QGISTest(unittest.TestCase):
         """QGIS environment has the expected providers"""
 
         r = QgsProviderRegistry.instance()
-        self.assertIn('gdal', r.providerList())
-        self.assertIn('ogr', r.providerList())
+        self.assertIn("gdal", r.providerList())
+        self.assertIn("ogr", r.providerList())
+        self.assertIn("postgres", r.providerList())
 
-if __name__ == '__main__':
+    def test_projection(self):
+        """Test that QGIS properly parses a wkt string."""
+        crs = QgsCoordinateReferenceSystem()
+        wkt = (
+            'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'
+            'SPHEROID["WGS_1984",6378137.0,298.257223563]],'
+            'PRIMEM["Greenwich",0.0],UNIT["Degree",'
+            "0.0174532925199433]]"
+        )
+        crs.createFromWkt(wkt)
+        auth_id = crs.authid()
+        expected_auth_id = "EPSG:4326"
+        self.assertEqual(auth_id, expected_auth_id)
+
+
+if __name__ == "__main__":
     unittest.main()
