49a4773f847db496d62929b88fd47d3848ffb874
Daniel <naturogfrihet@gmail.com>
2024-02-14 14:13:19 +0100

    edit test_suite

--- geovita_processing_plugin/test_suite.py
+++ geovita_processing_plugin/test_suite.py
@@ -53,14 +53,14 @@ def _run_tests(test_suite, package_name, with_coverage=False):
     if with_coverage:
         cov.stop()
         cov.save()
-        #report = tempfile.NamedTemporaryFile(delete=False)  # pylint: disable=consider-using-with
-        report = sys.stdout
+        report = tempfile.NamedTemporaryFile(delete=False)  # pylint: disable=consider-using-with
+        #report = sys.stdout
         cov.report(file=report)
         # Produce HTML reports in the `htmlcov` folder and open index.html
         # cov.html_report()
-        #report.close()
-        #with open(report.name, 'r', encoding='utf8') as fin:
-        #    print(fin.read())
+        report.close()
+        with open(report.name, 'r', encoding='utf8') as fin:
+            print(fin.read())
 
 
 def test_package(package='geovita_processing_plugin.test'):
